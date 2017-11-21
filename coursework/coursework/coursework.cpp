dd178a51b51c71d70f5733f1a6f879b7dfff7742
Matteo Drago <matteo.drago1994@gmail.com>
2017-11-21 14:50:29 +0000

    Slight change on the database writing structure. Now we don't write by column, but by row. Also, we don't write NaN values on DB (seems to speed up a lot the procedure)

--- coursework/coursework/coursework.cpp
+++ coursework/coursework/coursework.cpp
@@ -13,6 +13,7 @@
 #include <cmath>
 #include <algorithm>
 #include <chrono>
+#include <map>
 
 using namespace std;
 using namespace std::chrono;
@@ -84,9 +85,109 @@ double avg(vector<entry> v)
 	//return accumulate(v.begin(), v.end(), 0.0 / v.size());
 }
 
+double avg_value(const char* query, sqlite3 *db) {
+	sqlite3_stmt *statement;
+	double avg;
+	if (sqlite3_prepare_v2(db, query, -1, &statement, 0) == SQLITE_OK)
+	{
+		int cols = sqlite3_column_count(statement);
+		int result = 0;
+		while (true)
+		{
+			result = sqlite3_step(statement);
+			if (result == SQLITE_ROW)
+			{
+				avg = sqlite3_column_double(statement, 0);
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	sqlite3_finalize(statement);
+	return avg;
+}
+
+
 bool compareItems(const entry &a, const entry &b)
 {
 	return a.item < b.item || a.item == b.item && a.user < b.user;
+}
+
+int callback(void *data, int argc, char **argv, char **azColName) {
+	int i;
+	fprintf(stderr, "%s: ", (const char*)data);
+
+	for (i = 0; i < argc; i++) {
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+
+	printf("\n");
+	return 0;
+}
+
+int runTransaction(sqlite3 *db, vector<std::string> insert_query)
+
+{
+
+	if (!db)
+
+		return 0;
+
+	char *zErrMsg = 0;
+
+	// start sqlite transaction
+
+	sqlite3_exec(db, "BEGIN TRANSACTION", 0, 0, 0);
+
+	for (int i = 0; i < insert_query.size(); i++)
+	{
+		const char *query = insert_query[i].c_str();
+		//cout << query << std::endl;
+		int rc = sqlite3_exec(db, query, NULL, 0, &zErrMsg);
+		if (rc != SQLITE_OK) {
+			fprintf(stderr, "SQL error: %s\n", zErrMsg);
+			sqlite3_free(zErrMsg);
+			// rollback all update/insert to sqlite 
+			sqlite3_exec(db, "ROLLBACK", 0, 0, 0);
+			return 0; // if an error occur
+		}
+
+	}
+	// commit all to sqlite database
+	sqlite3_exec(db, "COMMIT", 0, 0, 0);
+	return 1;
+
+}
+
+/*
+int getSimilarityRows(sqlite3 *db) {
+	int rows;
+	char *query = "SELECT COUNT(user1) FROM similarity;";
+	sqlite3_stmt *statement;
+
+	if (sqlite3_prepare_v2(db, query, -1, &statement, 0) == SQLITE_OK)
+	{
+		int result = sqlite3_step(statement);
+		if (result == SQLITE_ROW)
+		{
+			rows = sqlite3_column_int(statement, 0);
+		}
+		else {
+			rows = 0;
+		}
+	}
+	sqlite3_finalize(statement);
+	return rows;
+}
+
+int getSimilarityColumns(sqlite3 *db) {
+	sqlite3_stmt *pStmt;
+	const char *tail;
+	char *columns_query = "SELECT * from similarity;";
+	sqlite3_prepare_v2(db, columns_query, strlen(columns_query), &pStmt, &tail);
+	return sqlite3_column_count(pStmt) - 1;
 }
 
 double pearson_similarity(int u1, int u2, sqlite3 *db)
@@ -161,80 +262,6 @@ double pearson_similarity(int u1, int u2, sqlite3 *db)
 
 	//cout << "Pearson coefficient: " << num / (sqrt(den1) * sqrt(den2)) << std::endl;
 	return num / (sqrt(den1) * sqrt(den2));
-}
-
-int callback(void *data, int argc, char **argv, char **azColName) {
-	int i;
-	fprintf(stderr, "%s: ", (const char*)data);
-
-	for (i = 0; i < argc; i++) {
-		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-	}
-
-	printf("\n");
-	return 0;
-}
-
-int runTransaction(sqlite3 *db, vector<std::string> insert_query)
-
-{
-
-	if (!db)
-
-		return 0;
-
-	char *zErrMsg = 0;
-
-	// start sqlite transaction
-
-	sqlite3_exec(db, "BEGIN TRANSACTION", 0, 0, 0);
-
-	for (int i = 0; i < insert_query.size(); i++)
-	{
-		const char *query = insert_query[i].c_str();
-		//cout << query << std::endl;
-		int rc = sqlite3_exec(db, query, NULL, 0, &zErrMsg);
-		if (rc != SQLITE_OK) {
-			fprintf(stderr, "SQL error: %s\n", zErrMsg);
-			sqlite3_free(zErrMsg);
-			// rollback all update/insert to sqlite 
-			sqlite3_exec(db, "ROLLBACK", 0, 0, 0);
-			return 0; // if an error occur
-		}
-
-	}
-	// commit all to sqlite database
-	sqlite3_exec(db, "COMMIT", 0, 0, 0);
-	return 1;
-
-}
-
-int getSimilarityRows(sqlite3 *db) {
-	int rows;
-	char *query = "SELECT COUNT(user1) FROM similarity;";
-	sqlite3_stmt *statement;
-
-	if (sqlite3_prepare_v2(db, query, -1, &statement, 0) == SQLITE_OK)
-	{
-		int result = sqlite3_step(statement);
-		if (result == SQLITE_ROW)
-		{
-			rows = sqlite3_column_int(statement, 0);
-		}
-		else {
-			rows = 0;
-		}
-	}
-	sqlite3_finalize(statement);
-	return rows;
-}
-
-int getSimilarityColumns(sqlite3 *db) {
-	sqlite3_stmt *pStmt;
-	const char *tail;
-	char *columns_query = "SELECT * from similarity;";
-	sqlite3_prepare_v2(db, columns_query, strlen(columns_query), &pStmt, &tail);
-	return sqlite3_column_count(pStmt) - 1;
 }
 
 void similarity_matrix(sqlite3 *db, sqlite3 *similarity, long int users, long int interval) {
@@ -540,15 +567,165 @@ void update_similarity_matrix(sqlite3 *db, sqlite3 *similarity, long int users, 
 
 	cout << "Final dimensions of similarity matrix: " << rows << "X" << cols << std::endl;
 }
+*/
+
+double pearson_similarity(vector<entry> user1, vector<entry> user2, double avg1, double avg2, sqlite3 *db)
+{
+	long int u1 = user1[0].user;
+	long int u2 = user2[0].user;
+	if (u1 == u2)
+	{
+		return 1.0;
+	}
+
+	user2.insert(user2.end(), user1.begin(), user1.end());
+	std::sort(user2.begin(), user2.end(), compareItems);
+
+	int matches = 0;
+	double newnum = 0;
+	double newden1 = 0;
+	double newden2 = 0;
+
+#pragma omp parallel for reduction(+:newnum), reduction(+:newden1), reduction(+:newden2);
+
+	for (int i = 0; i < user2.size() - 1; i++)
+	{
+		if (user2[i].item == user2[i + 1].item)
+		{
+			double avg_int1;
+			double avg_int2;
+			if (user2[i].user == u1)
+			{
+				avg_int1 = avg1;
+			}
+			else {
+				avg_int1 = avg2;
+			}
+			if (user2[i + 1].user == u1)
+			{
+				avg_int2 = avg1;
+			}
+			else {
+				avg_int2 = avg2;
+			}
+			double diff1 = (user2[i].rating - avg_int1);
+			double diff2 = (user2[i + 1].rating - avg_int2);
+			newnum += diff1 * diff2;
+			newden1 += pow(diff1, 2);
+			newden2 += pow(diff2, 2);
+			matches++;
+		}
+	}
+	return newnum / (sqrt(newden1) * sqrt(newden2));
+}
+
+
+
+void similarity_matrix(sqlite3 *db, sqlite3 *db_avg, sqlite3 *similarity, long int tot_users, long int interval) {
+
+	int check;
+	int trans_number = 1;
+	long int n = 1;
+	double sim; // pearson coefficient (from -1 to 1, if NaN we set it to a value of 10)
+	char *zErrMsg = 0;
+	const char *pzTest;
+	string temp_s;
+	vector<string> insert_query;
+	sqlite3_stmt *stmt;
+
+	std::cout << "Evaluate all pearson coefficient related to the first user and inserting them on the DB..." << std::endl;
+	string select1 = "SELECT * from usersratings WHERE usersratings.user = 1";
+
+	vector<entry> user1 = query(select1.c_str(), db);
+	map<long int, vector<entry>> users;
+
+	string select_avg1 = "SELECT avg from averages WHERE averages.user = 1";
+	double avg1 = avg_value(select_avg1.c_str(), db_avg);
+	map<long int, double> avgs;
+
+	for (long int i = 2; i <= tot_users; i++)
+	{
+		// Each user is saved also in a map in order to speed up the evaluation
+		string select = "SELECT * from usersratings WHERE usersratings.user = " + to_string(i);
+		users[i] = query(select.c_str(), db);
+		string select_avg = "SELECT avg from averages WHERE averages.user = " + to_string(i);
+		avgs[i] = avg_value(select_avg.c_str(), db_avg);
+		sim = pearson_similarity(user1, users[i], avg1, avgs[i], db);
+		if (!isnan(sim)) {
+			string to_insert = "INSERT INTO similarity(user1, user2, sim) VALUES(" + to_string(1) + ", " + to_string(i) + ", " + to_string(sim) + ")";
+			insert_query.push_back(to_insert);
+			if ((n % interval) == 0)
+			{
+				std::cout << "Transaction number " << trans_number << std::endl;
+				if (runTransaction(similarity, insert_query) == 0) {
+					break;
+				}
+				insert_query.clear();
+				trans_number++;
+			}
+			n++;
+		}
+	}
+
+	// Writing on DB what's left of the first user pearson coefficients
+	if (insert_query.size() > 0)
+	{
+		std::cout << "Transaction number " << trans_number << std::endl;
+		runTransaction(similarity, insert_query);
+		insert_query.clear();
+		trans_number++;
+	}
+
+	std::wcout << "Starting for the rest of the users..." << std::endl;
+
+	// Evaluate all the other columns
+
+	for (int i = 2; i <= tot_users; i++)
+	{
+		std::cout << "Writing row " << i << " ..." << std::endl;
+		for (int j = 2; j < i; j++)
+		{
+			//std::cout << "Comparison with " << j << " ..." << std::endl;
+			sim = pearson_similarity(users[i], users[j], avgs[i], avgs[j], db);
+			if (!isnan(sim)) {
+				string to_insert = "INSERT INTO similarity(user1, user2, sim) VALUES(" + to_string(i) + ", " + to_string(j) + ", " + to_string(sim) + ")";
+				insert_query.push_back(to_insert);
+				if ((n % interval) == 0)
+				{
+					std::cout << "Transaction number " << trans_number << std::endl;
+					if (runTransaction(similarity, insert_query) == 0) {
+						break;
+					}
+					insert_query.clear();
+					trans_number++;
+				}
+				n++;
+			}
+		}
+		//std::cout << "Finished row " << i << " ..." << std::endl;
+	}
+
+	// Writing on DB what's left on the array of queries
+	if (insert_query.size() > 0)
+	{
+		std::cout << "Transaction number " << trans_number << std::endl;
+		runTransaction(similarity, insert_query);
+		insert_query.clear();
+		trans_number++;
+	}
+}
+
+// --------------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------------
 
 int main(int argc, const char * argv[]) {
 
-	int rc, rc_2, check;
-	long int users = 2001;
+	int rc, rc_2, avgs;
+	long int users = 1000;
 	long int total_users;
 	long int commit_interval = 10000;
-	bool create = false; // Flag value: if create is false, we just update the similarity matrix that already exists.
-	sqlite3 *db, *db_2;
+	bool create = true; // Flag value: if create is false, we just update the similarity matrix that already exists.
+	sqlite3 *db, *db_2, *db_avgs;
 	sqlite3_stmt *stmt;
 	const char *queryTail;
 
@@ -572,6 +749,16 @@ int main(int argc, const char * argv[]) {
 		fprintf(stderr, "Opened database successfully\n");
 	}
 
+	// Open DB for averages
+	avgs = sqlite3_open("C:/sqlite/avgs", &db_avgs);
+	if (avgs != SQLITE_OK) {
+		fprintf(stderr, "Can't open averages database: %s\n", sqlite3_errmsg(db_avgs));
+		return 1;
+	}
+	else {
+		fprintf(stderr, "Opened database successfully\n");
+	}
+
 	// Get the total number of users
 	char *max_users_query = "SELECT * from usersratings WHERE user=(SELECT max(user) FROM usersratings)";
 	vector<entry> max = query(max_users_query, db);
@@ -580,14 +767,14 @@ int main(int argc, const char * argv[]) {
 
 	if (create)
 	{
-		char *creation_query = "CREATE TABLE similarity(userID INT NOT NULL UNIQUE, user1 DOUBLE);";
+		char *creation_query = "CREATE TABLE similarity(user1 int, user2 int, sim DOUBLE);";
 		if (sqlite3_prepare_v2(db_2, creation_query, -1, &stmt, &queryTail) == SQLITE_OK) {
 
 			sqlite3_step(stmt);
 			sqlite3_finalize(stmt);
 
 			high_resolution_clock::time_point t1 = high_resolution_clock::now();
-			similarity_matrix(db, db_2, users, commit_interval);
+			similarity_matrix(db, db_avgs, db_2, users, commit_interval);
 			high_resolution_clock::time_point t2 = high_resolution_clock::now();
 			auto duration = duration_cast<seconds>(t2 - t1).count();
 			cout << "Time to create similarity matrix: " << duration << std::endl;
@@ -595,14 +782,6 @@ int main(int argc, const char * argv[]) {
 		else {
 			cout << "An error occured. Database hasn't been created." << std::endl;
 		}
-	}
-	else {
-
-		high_resolution_clock::time_point t1 = high_resolution_clock::now();
-		update_similarity_matrix(db, db_2, users, commit_interval);
-		high_resolution_clock::time_point t2 = high_resolution_clock::now();
-		auto duration = duration_cast<seconds>(t2 - t1).count();
-		cout << "Time to update similarity matrix: " << s << std::endl;
 	}
 
 
